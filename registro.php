19b8413d2372caed5c08a33e9f9b8f64469b9b9d
zerojj <>
2013-02-06 21:51:03 +0100

    Modificado el htaccess y formulario registro

---
+++ registro.php
@@ -0,0 +1,114 @@
+  <div class="wrapper">
+
+	       <div class="grids top">
+
+		    <div class="grid-6 grid">
+			 <h2>Registrate</h2>
+			 <div>
+			      <p class="bottom">
+                                  En esta p&aacute;gina puedes registrate
+                              </p>
+			 </div>
+
+
+
+		    </div>
+
+
+
+
+		    <!--===============================================================  Contact form =====================================================================================-->
+		    <div class="grid-10 grid">
+
+			 <h2>Registro</h2>
+			 <form  action="#" method="post" enctype="multipart/form-data" onsubmit="return false">
+                              <table class="form">
+				   <tr>
+					<th>
+					     <label for="nick">
+						  Nick
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="text" id="nick" name="nick" required="required" />
+
+					</td>
+				   </tr>
+				   <tr>
+					<th>
+					     <label for="password">
+						  Password
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="password" id="password" name="password" required="required" />
+
+					</td>
+				   </tr>
+				   <tr>
+					<th>
+					     <label for="nombre">
+						  Nombre
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="text" id="nombre" name="nombre" required="required" />
+
+					</td>
+				   </tr>
+				   <tr>
+					<th>
+					     <label for="apellidos">
+						  Apellidos
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="text" id="apellidos" name="apellidos" required="required" />
+
+					</td>
+				   </tr>
+				   <tr>
+					<th>
+					     <label for="dni">
+						  DNI
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="text" id="dni" name="dni" required="required" />
+
+					</td>
+				   </tr>                                   
+				   <tr>
+					<th>
+					     <label for="e-mail">
+						  Email
+					     </label>
+					</th>
+					<td>
+
+					     <input class="input_full" type="email" id="e-mail" name="e-mail" required="required" />
+					</td>
+				   </tr>
+
+				   <tr>
+
+					<th>
+					     <label for="telefono">
+						  Tel&eacute;fono
+					     </label>
+					</th>
+					<td>
+					     <input class="input_full" type="tel" id="telefono" name="telefono" required="required" />
+					</td>
+				   </tr>
+                              </table>
+
+                              <p>
+				   <input type="submit" value="Send" class="float_left" />
+				   <input type="reset" value="Reset" class="float_right">
+                              </p>
+			 </form>
+
+
+		    </div><!--end of grid-10-->
+	       </div><!--end of grids-->
